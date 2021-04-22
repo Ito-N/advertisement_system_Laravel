051891a9a74ee54a919bafed4059366b97fc53f5
Ito-N <theme4087abcdef@yahoo.co.jp>
2021-04-22 10:03:44 +0900

    feat: Bootstrap Carouselを使ってホーム画面実装

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-md navbar-light bg-danger shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
@@ -75,9 +75,60 @@
         </div>
     </nav>
 
+    <!-- second navbar -->
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm navbar-hover">
+
+            <a class="navbar-brand" href="">
+
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHover" aria-controls="navbarDD" aria-expanded="false" aria-label="Navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+                @for($i = 0; $i < 12; $i++)
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="" data-toggle="dropdown_remove_dropdown_class_for_clickable_link"
+                        arial-haspopup="true"
+                        arial-expanded="false">
+                            Category
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item dropdown-toggle" href="">Subcategory</a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a class="dropdown-item" href="">ChildCategory</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                @endfor
+            </ul>
+        </div>
+    </nav>
+    <!--end -->
+
     <main class="py-4">
         @yield('content')
     </main>
 </div>
+<style>
+    .dropdown:hover>.dropdown-menu{
+        display: block;
+    }
+    @media only screen and (max-width: 9991px) {
+        .navbar-hover .show > .dropdown-toggle::after{
+            transform: rotate(-90deg);
+        }
+    }
+    @media only screen and (min-width: 492px) {
+        .navbar-hover .collapse ul li{position: relative;}
+        .navbar-hover .collapse ul li:hover > ul {display: block;}
+        .navbar-hover .collapse ul ul{position: absolute; top: 100%; left: 0; min-width: 250px; display: none;}
+        .navbar-hover .collapse ul ul ul{position: absolute; top: 0; left: 100%; min-width: 250px; display: none;}
+    }
+</style>
 </body>
 </html>
